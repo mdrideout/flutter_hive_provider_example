66243b36116d259315d3a5d08cb0e8cedd992eb0
Matthew Rideout <matt@matts-2018-mbp.lan>
2019-10-29 19:51:39 -0400

    Refactored Hive implementation to include the type parameter with openBox, instead of casting it after reading values.

--- lib/models/contact_data.dart
+++ lib/models/contact_data.dart
@@ -16,10 +16,10 @@ class ContactsData extends ChangeNotifier {
   /// Get Contacts
   /// Gets all contacts from the hive box and loads them into our state List
   void getContacts() async {
-    var box = await Hive.openBox(_boxName);
+    var box = await Hive.openBox<Contact>(_boxName);
 
     // Update our provider state data with a hive read, and refresh the ui
-    _contacts = box.values.toList().cast<Contact>();
+    _contacts = box.values.toList();
     notifyListeners();
   }
 
@@ -40,13 +40,13 @@ class ContactsData extends ChangeNotifier {
   /// - Updates our List with the hive data by read
   /// - Notifies listeners to update the UI, which will be a consumer of the _contacts List
   void addContact(Contact newContact) async {
-    var box = await Hive.openBox(_boxName);
+    var box = await Hive.openBox<Contact>(_boxName);
 
     // Add a contact to our box
     await box.add(newContact);
 
     // Update our provider state data with a hive read, and refresh the ui
-    _contacts = box.values.toList().cast<Contact>();
+    _contacts = box.values.toList();
     notifyListeners();
   }
 
@@ -55,7 +55,7 @@ class ContactsData extends ChangeNotifier {
   /// - Updates our List from Hive
   /// - Notifies listeners to update the UI
   void deleteContact(index) async {
-    var box = await Hive.openBox(_boxName);
+    var box = await Hive.openBox<Contact>(_boxName);
 
     print("Box Keys: " + box.keys.toList().toString());
     print("Deleting Index " +
@@ -70,7 +70,7 @@ class ContactsData extends ChangeNotifier {
     await box.delete(boxKey);
 
     // Update our provider state data with a hive read, and refresh the ui
-    _contacts = box.values.toList().cast<Contact>();
+    _contacts = box.values.toList();
     notifyListeners();
   }
 }
